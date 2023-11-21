2a1d56d992e58bf871c1953e0fcd40fbe1012046
Jonathan Hollingsworth <jonathanhollingsworth07@gmail.com>
2023-11-21 17:58:34 -0500

    Created functionality for `insertionSort`

--- insertion.js
+++ insertion.js
@@ -1,3 +1,15 @@
-function insertionSort() {}
+function insertionSort(arr) {
+    for (let i = 1; i < arr.length; i++) {
+        val = arr[i]
+        for (let j = i-1; j >= 0; j--) {
+            if(arr[j] > val) {
+                arr[j+1] = arr[j]
+                arr[j] = val
+            } else { break }
+        }
+    }
+
+    return arr
+}
 
 module.exports = insertionSort;
