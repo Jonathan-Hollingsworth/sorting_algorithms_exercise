b57f664e969bf9dbcc45e074dd75ab794ec47fcd
Jonathan Hollingsworth <jonathanhollingsworth07@gmail.com>
2023-11-21 16:19:59 -0500

    Created functionality for bubbleSort

--- bubble.js
+++ bubble.js
@@ -1,3 +1,18 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let hasMoved = false
+        for (let j = 0; j < arr.length - i; j++) {
+            if (arr[j] > arr[j+1]) {
+                let temp = arr[j]
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
+                hasMoved = true
+            }
+        }
+        if (!hasMoved) break
+    }
+
+    return arr
+}
 
 module.exports = bubbleSort;
